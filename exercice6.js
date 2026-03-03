9a9c5cd2e65fa702f3693b03e0ff41a06517d39e
Hmissou <o.hmissou6454@uca.ac.ma>
2026-03-03 17:00:25 +0000

    TP2 javascript

---
+++ exercice6.js
@@ -0,0 +1,29 @@
+let nom = null;
+let age = 0;
+let ville = "";
+let score = undefined;
+let actif = false;
+
+console.log(`nom ?? "valeur par défaut" -> ${nom ?? "valeur par défaut"}`);
+console.log(`age ?? "valeur par défaut" -> ${age ?? "valeur par défaut"}`);
+console.log(`ville?? "valeur par défaut" -> ${ville?? "valeur par défaut"}`);
+console.log(`score?? "valeur par défaut" -> ${score?? "valeur par défaut"}`);
+console.log(`actif?? "valeur par défaut" -> ${actif?? "valeur par défaut"}`);
+
+console.log("\n");
+
+console.log(`nom || "valeur par défaut" -> ${nom || "valeur par défaut"}`);
+console.log(`age || "valeur par défaut" -> ${age || "valeur par défaut"}`);
+console.log(`ville || "valeur par défaut" -> ${ville || "valeur par défaut"}`);
+console.log(`score || "valeur par défaut" -> ${score || "valeur par défaut"}`);
+console.log(`actif || "valeur par défaut" -> ${actif || "valeur par défaut"}`);
+
+console.log("\n");
+
+console.log(`nom || "valeur par défaut" -> ${nom || "valeur par défaut"}`);
+console.log(`age || "valeur par défaut" -> ${age || "valeur par défaut"}`);
+console.log(`ville || "valeur par défaut" -> ${ville || "valeur par défaut"}`);
+console.log(`score || "valeur par défaut" -> ${score || "valeur par défaut"}`);
+console.log(`actif || "valeur par défaut" -> ${actif || "valeur par défaut"}`);
+
+console.log("\n");
